df75d8e3500b3947ecdfa3fa7a877baa84cb6917
Andrea G <andrea.galinovic@gmail.com>
2023-01-23 19:09:10 +0100

    prva verzija primjera s nizom

---
+++ pyhton/igranje nizom.py
@@ -0,0 +1,20 @@
+#crtanje znakovima
+
+#unos vrijednosti
+
+rijec=input('rijec=')
+
+#ispis gornjeg okvira
+znak='-'
+sirina=len('->')+len(rijec)
+print(znak*sirina*3)
+
+#ispis teksta
+tekst='->'+rijec
+
+print(tekst*1)
+print(tekst*2)
+print(tekst*3)
+
+#ispis doljeneg okvira
+print(znak*sirina*3)
