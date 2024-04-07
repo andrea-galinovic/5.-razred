2f38133cb4df3e791b4e765c4cc1840a7d5c708d
Andrea G <andrea.galinovic@gmail.com>
2024-04-07 17:35:50 +0200

    elif umjesti else if

--- pyhton/granjanje/obavezni elif.py
+++ pyhton/granjanje/obavezni elif.py
@@ -9,28 +9,17 @@ print('boja je:',boja)
 #provjera da li je boja plava
 if boja=='plava':
     print('engleski naziv je blue')
-else:
-    if boja=='crvena':
-        print('engleski naziv je red')
-    else:
-        if boja=='zelena':
-            print('engleski naziv je green')
-        else:
-            if boja=='bjela':
-                print('engleski naziv je white')
-            else:
-                if boja=='crna':
-                    print('engleski naziv je black')
-                else:
-                    if boja=='ljubicasta':
-                        print('engleski naziv je purpule')
-                    else:
-                        if boja=='roza':
-                            print('engleski naziv je pink')
-                        else:
-                            if boja=='zuta':
-                                print('engleski naziv je yellow')
-                                
-                        
-                    
-
+elif boja=='crvena':
+    print('engleski naziv je red')
+elif boja=='zelena':
+    print('engleski naziv je green')
+elif boja=='bjela':
+    print('engleski naziv je white')
+elif boja=='crna':
+    print('engleski naziv je black')
+elif boja=='ljubicasta':
+    print('engleski naziv je purpule')
+elif boja=='roza':
+    print('engleski naziv je pink')
+elif boja=='zuta':
+    print('engleski naziv je yellow')
