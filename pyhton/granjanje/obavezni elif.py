b3d51f8a07e8ff9074eaf0c41a10b00eecad299f
Andrea G <andrea.galinovic@gmail.com>
2024-04-07 17:42:05 +0200

    manji refactoring

--- pyhton/granjanje/obavezni elif.py
+++ pyhton/granjanje/obavezni elif.py
@@ -6,20 +6,25 @@ boja=input('unesi ime boje na hrvatskom:')
 #ispisi boju na hrvatskom
 print('boja je:',boja)
 
+#definiranje pocetnog teksta
+tekst='engleski naziv je'
+
 #provjera da li je boja plava
 if boja=='plava':
-    print('engleski naziv je blue')
+    print(tekst,'blue')
 elif boja=='crvena':
-    print('engleski naziv je red')
+    print(tekst,'red')
 elif boja=='zelena':
-    print('engleski naziv je green')
+    print(tekst,'green')
 elif boja=='bjela':
-    print('engleski naziv je white')
+    print(tekst,'white')
 elif boja=='crna':
-    print('engleski naziv je black')
+    print(tekst,'black')
 elif boja=='ljubicasta':
-    print('engleski naziv je purpule')
+    print(tekst,'purpule')
 elif boja=='roza':
-    print('engleski naziv je pink')
+    print(tekst,'pink')
 elif boja=='zuta':
-    print('engleski naziv je yellow')
+    print(tekst,'yellow')
+else:
+    print('boja mi nije poznata')
