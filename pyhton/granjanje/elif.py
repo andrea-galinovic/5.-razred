c29af1bc3e3bb34a624eec7f962e495b0a112b5b
Andrea G <andrea.galinovic@gmail.com>
2024-04-07 17:30:58 +0200

    elif zadaci

---
+++ pyhton/granjanje/elif.py
@@ -0,0 +1,17 @@
+#zadatak 8 udbenik strana 67
+
+#unos prvog broja
+a=int(input('unesi prvi broj:'))
+
+#unos drugog broja
+b=int(input('unesi drugi broj:'))
+
+#provjera jesu li brojevi jednaki
+if a==b:
+    print('brojevi',a,',',b, "jednaki")
+else:
+    if a<b:
+        print('broj',a,'je manji od',b)
+    else:
+        print('broj',a,'je veci od',b)
+    
---
+++ pyhton/granjanje/obavezni elif.py
@@ -0,0 +1,36 @@
+#unesi boju na hrvatskom i ispisi ime boje na engleskom
+
+#unesi boju
+boja=input('unesi ime boje na hrvatskom:')
+
+#ispisi boju na hrvatskom
+print('boja je:',boja)
+
+#provjera da li je boja plava
+if boja=='plava':
+    print('engleski naziv je blue')
+else:
+    if boja=='crvena':
+        print('engleski naziv je red')
+    else:
+        if boja=='zelena':
+            print('engleski naziv je green')
+        else:
+            if boja=='bjela':
+                print('engleski naziv je white')
+            else:
+                if boja=='crna':
+                    print('engleski naziv je black')
+                else:
+                    if boja=='ljubicasta':
+                        print('engleski naziv je purpule')
+                    else:
+                        if boja=='roza':
+                            print('engleski naziv je pink')
+                        else:
+                            if boja=='zuta':
+                                print('engleski naziv je yellow')
+                                
+                        
+                    
+
